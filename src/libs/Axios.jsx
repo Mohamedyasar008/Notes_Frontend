994f9390208d78f4b822134e683290f9f950b9e0
Mohammed yasar <mhdyasxr@gmail.com>
2025-09-08 01:30:06 +0530

    Update Axios config and add gitignore

--- src/libs/Axios.jsx
+++ src/libs/Axios.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 // });
 
 const api = axios.create({
-  baseURL: "https://notes-backend-0njc.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001/api",
 });
 
 
